ac4611e08ba3b6d9ac1cce4110bf7fdfe0ab29e4
Charlotte <dan.jones8902@gmail.com>
2021-05-14 16:01:20 +0100

    Recursion
    Also learned that  functions with code in them declared above main() don't have the declaration. This seems like trivial stuff, but coming from Unity it's important to klnow.

--- Cpp_Beginings/HelloWorld.cpp
+++ Cpp_Beginings/HelloWorld.cpp
@@ -5,18 +5,23 @@
 
 using namespace std; // std has things like string
 
-void Print(string);
+// Recursive functions
+int f(int n)
+{
+    // Base function to stop never ending calling
+    if (n <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * f(n - 1);
+    }
+}
 
 
 int main()
 {
-    Print("Hello World");
-    Print("You beurtiful princess!");
-    
-}
-
-
-void Print(string output)
-{
-    cout << output << endl;
+    cout << f(5) << endl;
+    return 0;
 }
