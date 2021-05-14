a3ba49e2e0d8a2a272a2ca486eecf5ab514d4c24
Charlotte <dan.jones8902@gmail.com>
2021-05-14 16:26:36 +0100

    Multidimension arrays and user input

--- Cpp_Beginings/HelloWorld.cpp
+++ Cpp_Beginings/HelloWorld.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+#include <string>
+
 
 using namespace std; // std has things like string
 
@@ -20,8 +22,33 @@ int f(int n)
 }
 
 
+// Multi dimensional arrays
+int arr2D[2][3] = { {10, 11, 12}, {20, 21, 22} };
+int arr3d[2][3][2] = {
+    { {1,-1}, {2, -2}, {3,-1} },
+    { {4,-4}, {5, -5}, {6,-6} }
+};
+
+
+
 int main()
 {
-    cout << f(5) << endl;
+    //cout << f(5) << endl;
+
+    // User Input
+    string str;
+    cout << "Enter a string: ";
+    
+    getline(cin, str); // <string>
+    cout << "You entered: ";
+    cout << str << endl;
+
+    // This function add a character at the end of the string
+    str.push_back('A');
+    cout << "The string after push back: " << str << endl;
+
+    str.pop_back();
+    cout << "the string after pop_back: " << str << endl;
+
     return 0;
 }
